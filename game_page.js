b20ca956c612ebbe3230aff8a8420173fc0e28cc
taran-duba <72531052+taran-duba@users.noreply.github.com>
2021-01-12 17:56:14 -0500

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -3,13 +3,16 @@ var player1_score = 0;
 var player2_score = 0;
 var player1_name = localStorage.getItem("player1_name");
 var player2_name = localStorage.getItem("player2_name");
+document.getElementById("player1_name").innerHTML = player1_name + ": ";
+document.getElementById("player2_name").innerHTML = player2_name + ": ";
 function send() {
     var number1 = document.getElementById("number1").value;
     var number2 = document.getElementById("number2").value;
     actual_answer = parseInt(number1) * parseInt(number2);
     var question_number = "<h4>" + number1 + " x " + number2 + "</h4>";
     var input_box = "<br>Answer: <input type='text' id='input_check_box'>";
-    var row = actual_answer + question_number + input_box;
+    var check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>"
+    var row = question_number + input_box + check_button;
     document.getElementById("output").innerHTML = row;
     document.getElementById("number1").value = "";
     document.getElementById("number2").value = "";
